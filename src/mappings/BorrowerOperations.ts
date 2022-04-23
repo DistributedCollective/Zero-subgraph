c97c9208fa09ecc46d25dd76832ba766a658f32a
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-04-23 20:58:52 +0300

    Initial commit

---
+++ src/mappings/BorrowerOperations.ts
@@ -0,0 +1,25 @@
+import {
+  TroveUpdated,
+  ZUSDBorrowingFeePaid
+} from "../../generated/BorrowerOperations/BorrowerOperations";
+
+import { getTroveOperationFromBorrowerOperation } from "../types/TroveOperation";
+
+import { setBorrowingFeeOfLastTroveChange, updateTrove } from "../entities/Trove";
+import { increaseTotalBorrowingFeesPaid } from "../entities/Global";
+
+export function handleTroveUpdated(event: TroveUpdated): void {
+  updateTrove(
+    event,
+    getTroveOperationFromBorrowerOperation(event.params.operation),
+    event.params._borrower,
+    event.params._coll,
+    event.params._debt,
+    event.params.stake
+  );
+}
+
+export function handleLUSDBorrowingFeePaid(event: ZUSDBorrowingFeePaid): void {
+  setBorrowingFeeOfLastTroveChange(event.params._ZUSDFee);
+  increaseTotalBorrowingFeesPaid(event.params._ZUSDFee);
+}
