fe3f700393ce1b03d008d6b189ac60575178b5d7
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2022-08-09 13:38:01 +0100

    Feat/new deployment (#2)
    * Updated addresses and mainnet addresses
    
    * Correct start blocks

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -1,2 +1,2 @@
-export const activePoolAddress = "0x4AaffD5a31dEe60c1a07fCf93a012666310A155a";
-export const stabilityPoolAddress = "0x0DcedF5e080ED1D58B27b030d042D60971408D26";
+export const activePoolAddress = "0xd7d6cF12FEb339Dc99a6d5b7a54bDD37953fc937";
+export const stabilityPoolAddress = "0xd6eD2f49D0A3bF20126cB78119c7CB24D02d605F";
