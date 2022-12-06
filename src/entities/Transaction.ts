a19459915eb43fdb49e9148dfd2f99801189fde2
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-12-06 13:52:21 -0300

    Upgraded graph-ts version

--- src/entities/Transaction.ts
+++ src/entities/Transaction.ts
@@ -16,10 +16,23 @@ export function getTransaction(event: ethereum.Event): Transaction {
     return transactionOrNull as Transaction;
   } else {
     let newTransaction = new Transaction(transactionId);
-
     newTransaction.sequenceNumber = getTransactionSequenceNumber();
     newTransaction.blockNumber = event.block.number.toI32();
     newTransaction.timestamp = event.block.timestamp.toI32();
+    newTransaction.gasPrice = event.transaction.gasPrice;
+    newTransaction.index = event.transaction.index.toI32();
+    newTransaction.from = event.transaction.from;
+    newTransaction.to = event.transaction.to;
+    newTransaction.value = event.transaction.value;
+    newTransaction.gasLimit = event.transaction.gasLimit;
+    const receipt = event.receipt;
+    if (receipt != null) {
+      newTransaction.gasUsed = receipt.gasUsed;
+    }
+
+    newTransaction.functionSignature = event.transaction.input
+      .toHexString()
+      .slice(0, 10);
     newTransaction.save();
 
     return newTransaction;
